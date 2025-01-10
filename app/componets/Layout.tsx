8e42bf1b85cead359cdc67c846053c3d5e5985bf
Gazi2050 <gazi45344@gmail.com>
2025-01-11 00:20:23 +0600

    update configaration

--- app/componets/Layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="h-screen w-full bg-yellow-100">
-            {children}
-        </div>
-    );
-}
